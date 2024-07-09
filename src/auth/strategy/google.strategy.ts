6699399104defb1f8f53e310fa7945f9dc0f78e4
tachyon6 <songjunjun62754@gmail.com>
2024-07-09 15:24:32 +0900

    feat: google 로그인 로직

---
+++ src/auth/strategy/google.strategy.ts
@@ -0,0 +1,32 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
+import { Injectable } from '@nestjs/common';
+import { AuthService } from '../auth.service';
+
+// 구글 로그인 전략을 정의합니다.
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  // AuthService를 주입합니다.  
+  constructor(private authService: AuthService) {
+    super({
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+      scope: ['email', 'profile'],
+    });
+  }
+
+  // 구글 로그인 성공 시 호출됩니다.
+  async validate(accessToken: string, refreshToken: string, profile: any, done: VerifyCallback) {
+    const { displayName, emails } = profile;
+    const userPayload = {
+      email: emails[0].value,
+      name: displayName,
+      accessToken,
+      refreshToken,
+    };
+
+    const user = await this.authService.validateOAuthLogin(userPayload, 'google');
+    done(null, user);
+  }
+}
