43e0559f613e6e043580c619991bd3481099b147
Atipico1 <athjk3@naver.com>
2024-07-18 13:21:43 +0900

    Feat: claude API Call

---
+++ src/document/dto/create-document-contents.dto.ts
@@ -0,0 +1,10 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+// CreatePostDto는 게시글을 생성할 때 사용하는 데이터 전송 객체입니다.
+export class CreateDocumentContentsDto {
+  @ApiProperty({
+    example: 1,
+    description: 'The id of the document',
+  })
+  documentId: number;
+}
--- src/file/file.module.ts
+++ src/file/file.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FileController } from './file.controller';
 import { FileService } from './file.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { File } from './entity/file.entity';
+import { Document } from 'src/document/entity/document.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([File, Document])],
   controllers: [FileController],
   providers: [FileService]
 })
