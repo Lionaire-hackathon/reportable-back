1da5f42419904e9478924ed0cea8839515f8d56d
tachyon6 <songjunjun62754@gmail.com>
2024-07-19 20:58:46 +0900

    feat: 가드 추가

--- src/document/document.controller.ts
+++ src/document/document.controller.ts
@@ -21,13 +21,13 @@ export class DocumentController {
   constructor(private readonly documentService: DocumentService) {}
 
   // 새로운 문서를 생성합니다.
-  //@UseGuards(JwtGuard) // JwtGuard를 사용하여 JWT를 검증합니다.
+  @UseGuards(JwtGuard) // JwtGuard를 사용하여 JWT를 검증합니다.
   @Post()
   create(@Body() createDocumentDto: CreateDocumentDto, @Req() req) {
-    return this.documentService.create(createDocumentDto, 1);
+    return this.documentService.create(createDocumentDto, req.user.userId);
   }
   // 문서의 내용을 생성합니다.
-  //@UseGuards(JwtGuard) // JwtGuard를 사용하여 JWT를 검증합니다.
+  @UseGuards(JwtGuard) // JwtGuard를 사용하여 JWT를 검증합니다.
   @Put('content/:id')
   createContent(@Param('id') documentId: number) {
     return this.documentService.createContent(documentId);
@@ -38,12 +38,13 @@ export class DocumentController {
     return this.documentService.firstPrompt(documentId);
   }
   // 문서의 내용 수정하기
-  //@UseGuards(JwtGuard) // JwtGuard를 사용하여 JWT를 검증합니다.
+  @UseGuards(JwtGuard) // JwtGuard를 사용하여 JWT를 검증합니다.
   @Patch('edit')
   edit(@Body() editDocumentDto: EditDocumentDto) {
     return this.documentService.edit(editDocumentDto);
   }
   
+  @UseGuards(JwtGuard) // JwtGuard를 사용하여 JWT를 검증합니다.
   @Get('text/:id')
   getText(@Param('id') documentId: number) {
     return this.documentService.getText(documentId);
